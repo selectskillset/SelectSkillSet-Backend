8f71c7ce23b617a37fc8e643d3ab64249abbf5fc
suleman <selectskillset@gmail.com>
2025-06-23 11:15:12 +0530

    fix: update email transport configuration to use hostinger SMTP settings

--- helper/emailService.js
+++ helper/emailService.js
@@ -4,16 +4,14 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp.hostinger.com", 
+  port: 465, 
+  secure: true, 
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: process.env.SMTP_USER, 
+    pass: process.env.SMTP_PASS, 
   },
 });
-
 
 
 const sendEmail = async (toEmail, subject, text, html) => {
