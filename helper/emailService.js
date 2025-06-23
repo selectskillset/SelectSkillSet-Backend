3be51342a47ff42ca955dcf2f4b67bb1a10ab4a7
suleman <selectskillset@gmail.com>
2025-06-23 11:19:20 +0530

    fix: correct formatting and ensure consistent use of semicolons in emailService.js

--- helper/emailService.js
+++ helper/emailService.js
@@ -1,18 +1,17 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.hostinger.com", 
-  port: 465, 
-  secure: true, 
+  host: "smtp.hostinger.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.SMTP_USER, 
-    pass: process.env.SMTP_PASS, 
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
-
 
 const sendEmail = async (toEmail, subject, text, html) => {
   const mailOptions = {
